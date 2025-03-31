6eaffd2f4ddb1ca62a9269f8d5b4d197e66a5a7d
Satyam <sm2003satyam@gmail.com>
2025-03-31 11:44:41 +0530

    add users and refresh_tokens tables with necessary fields

--- src/data/data.sql
+++ src/data/data.sql
@@ -1,8 +1,19 @@
+-- Users Table
 CREATE TABLE IF NOT EXISTS users (
+    id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username VARCHAR(30) NOT NULL,
     email VARCHAR(30) UNIQUE NOT NULL,
-    password VARCHAR(12) NOT NULL,
-    avatar TEXT ,
-    coverimage TEXT DEFAULT '',
-    refreshToken TEXT DEFAULT ''
+    password TEXT NOT NULL,
+    avatar TEXT,
+    coverimage TEXT,
+    created_at NOT NULL DEFAULT NOW()
+)
+
+-- Refresh Tokens Table
+CREATE TABLE IF NOT EXISTS refresh_tokens (
+    id SERIAL PRIMARY KEY,
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    token TEXT NOT NULL UNIQUE,
+    expires_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
 )
